37f71364bad57da922e8aac32a624319ff37d61e
sunzheng19911227 <452017791@qq.com>
2016-01-25 19:01:21 +0800

    test

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+# some normal code
+# ...
+# ...
+# a bug here
+# fixed buggggg!
+# fixed buggggg!
+# some normal code
+# ...
+# ...
+# function user_new
+#  done 2 233
+# function over
+
+?>
